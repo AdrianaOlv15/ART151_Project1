e1be155ced82d659b8aade185934cf1ad3845845
AdrianaOlv15 <aolver6@uic.edu>
2022-09-25 21:57:48 -0500

    push final drawing

--- sketch.js
+++ sketch.js
@@ -4,17 +4,79 @@ let xincrement = 5;
 let yoff = 0.1;
 let yincrement = 5;
 
+let backColor = 0;
+let frontColor = 0;
+
+let count = 0; 
+let count2 = 0;
+
+let xposition;
+let yposition;
+
+let x = 320;
+let y = 180;
+
+let xspeed = 10;
+let yspeed = 5;
+
+let r = 5;
 
 function setup(){
     createCanvas(windowWidth,windowHeight);
-    background(0);
+    background(backColor);
     noStroke();
+}
+
+
+function drawCircle (myColor, myColor2, xposition, yposition, nextSize , nextSize ){
+    fill(myColor);
+    noStroke();
+    ellipse(xposition, yposition, nextSize , nextSize); //100
+
+    // smaller circle inside
+    for (let i = 0; i < 11; i++){
+        noStroke();
+        // calculate smaller circle size
+            nextSize = nextSize - nextSize/10;
+        // alternate colors
+            if ( (i%2) == 0 ) { fill(myColor2); }
+            else { fill(myColor); }
+        // draw circle
+            frameRate(15);
+            noStroke();
+            ellipse(xposition, yposition, nextSize , nextSize);
+    } 
 }
 
 function draw(){
 
+    fill(255)
+    ellipse(x, y, r*2, r*2);
+    x += xspeed;
+    y += yspeed;
+    if (x > width - r || x < r) {
+        xspeed = -xspeed;
+    }
+    if (y > height - r || y < r) {
+        yspeed = -yspeed;
+    }
+
     // create an alpha blended background
-    fill(0,10);
+    if (mouseIsPressed === true && mouseX > width*0.47 && mouseX < width*0.53 && mouseY > height*0.47 && mouseY < height*0.53) {
+        fill(0);
+        rect(0, 0, width, height);
+        count++;        
+    }
+
+    if (count%3 == 0) {
+        frontColor = color(0, random(0,255), random(0,255));
+    } else if (count%3 == 1) {
+        frontColor = color(random(200,255), random(0,255), 0);
+    }else if (count%3 == 2) {
+        frontColor = color(random(200,255), 0, random(0,255));
+    }     
+
+    fill(backColor,10);
     rect(0,0,width, height);
 
     xoff += xincrement;
@@ -22,33 +84,47 @@ function draw(){
 
     // color of the circles
     //let myColor = color(92, 255, 228);
-    let myColor = color(0, random(0,200), random(0,200));
+    let myColor = color(frontColor);
+    let myColor2 =  color(backColor);
 
     // range of sizes for the circles
-    nextSize = random(100, 200);
-
+    nextSize = random(50, 100);
+    
     // random postion of circle
-    xposition = random(0, width);
-    yposition = random(0, height)
+    //xposition = random(0, width * 0.4);
+    //yposition = random(0, height)
 
-    // 1st circle - color
-    fill(myColor);
-    ellipse(xposition, yposition, nextSize , nextSize); //100
+    section = Math.floor(Math.random() * 101);
 
-    // smaller circle inside
-    for (let i = 0; i < 50; i++){
+    console.log("section = ", section%4);
 
-        let myColor = color(0, random(0,200), random(0,200));
+    xposition = random(0, width); // right
+    yposition = random(0, height);
 
-        // calculate smaller circle size
-            nextSize = nextSize - nextSize/10;
-        // alternate colors
-            if ( (i%2) == 0 ) { fill(0,0,0); }
-            else { fill(myColor); }
-        // draw circle
-            frameRate(15);
-            ellipse(xposition, yposition, nextSize , nextSize);
-    }   
+    while (xposition < width * 0.7 && xposition > width * 0.3 && yposition < height*0.7 && yposition > height*0.3){
+        xposition = random(0, width); 
+        yposition = random(0, height);
+    }
+    
+    
+
+    // draw circle 
+    drawCircle (myColor, myColor2, xposition, yposition, nextSize , nextSize );
+    drawCircle (0, 0, width*0.5, height*0.5, 150 , 150);
+
+    let nextC = 0;
+
+    if ((count+1)%3 == 0) {
+        nextC = color(0, random(0,255), random(0,255));
+    } else if ((count+1)%3 == 1) {
+        nextC = color(random(0,255), random(0,255), 0);
+
+    } else if ((count+1)%3 == 2) {
+        nextC = color(random(0,255), 0, random(0,255));
+    } 
+
+    let newSize = random(50,50);
+    drawCircle (nextC, myColor2, width*0.5, height*0.5, newSize, newSize);
 }
 
 
